101bbe016b9d2c6d323c7527e073743a38ab5bab
TeeshMaster <tshmaster@gmail.com>
2024-01-01 20:44:47 +1300

    move asset definitions

--- dagster_project/assets/__init__.py
+++ dagster_project/assets/__init__.py
@@ -1,4 +1,4 @@
-from dagster import AssetKey, AssetExecutionContext
+from dagster import AssetKey, AssetExecutionContext, load_assets_from_package_module
 
 from dagster_dbt import (
     DagsterDbtTranslator,
@@ -6,12 +6,22 @@ from dagster_dbt import (
     dbt_assets,
 )
 
+from ..assets import analytics
+
 from pathlib import Path
 from typing import Any, Mapping
 
 DBT_PROJECT_DIR = Path(__file__).joinpath("..", "..", "..", "dbt_project").resolve()
 
 manifest_path = DBT_PROJECT_DIR.joinpath("target", "manifest.json")
+
+
+analytics_assets = load_assets_from_package_module(
+    package_module=analytics,
+    group_name="analytics",
+    key_prefix=["snowflake", "analytics"]
+)
+
 
 class CustomDagsterDbtTranslator(DagsterDbtTranslator):
     def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
--- dagster_project/resources/__init__.py
+++ dagster_project/resources/__init__.py
@@ -1,20 +1,13 @@
 import os
 
-from dagster import load_assets_from_package_module
 from dagster._utils import file_relative_path
 from dagster_dbt import DbtCliResource
 from dagster_snowflake_pandas import SnowflakePandasIOManager
 
-from ..assets import analytics
 
 DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_project")
 DBT_PROFILES_DIR = file_relative_path(__file__, "../../dbt_project/")
 
-analytics_assets = load_assets_from_package_module(
-    package_module=analytics,
-    group_name="analytics",
-    key_prefix=["snowflake", "analytics"]
-)
 
 RESOURCES_DEV = {
     "snowflake_io_manager": SnowflakePandasIOManager(
